66b62cfd15eef6d7208f95bf98dcabdd6bf11880
bayashi <bayashi@cpan.org>
2016-05-20 10:26:43 +0900

    bump version

--- lib/Plack/Middleware/RequestId.pm
+++ lib/Plack/Middleware/RequestId.pm
@@ -12,7 +12,7 @@ use Plack::Util::Accessor qw/
     id_generator
 /;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 our $request_id;
 
@@ -124,7 +124,7 @@ Normally, you get the request ID from PSGI env. However, the ID has been stored 
 
 =begin html
 
-<a href="http://travis-ci.org/bayashi/Plack-Middleware-RequestId"><img src="https://secure.travis-ci.org/bayashi/Plack-Middleware-RequestId.png?_t=1443672845"/></a> <a href="https://coveralls.io/r/bayashi/Plack-Middleware-RequestId"><img src="https://coveralls.io/repos/bayashi/Plack-Middleware-RequestId/badge.png?_t=1443672845&branch=master"/></a>
+<a href="http://travis-ci.org/bayashi/Plack-Middleware-RequestId"><img src="https://secure.travis-ci.org/bayashi/Plack-Middleware-RequestId.png?_t=1463707503"/></a> <a href="https://coveralls.io/r/bayashi/Plack-Middleware-RequestId"><img src="https://coveralls.io/repos/bayashi/Plack-Middleware-RequestId/badge.png?_t=1463707503&branch=master"/></a>
 
 =end html
 
